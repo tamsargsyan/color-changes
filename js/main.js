d9808bfc9812677a5c24a03b969041aba41e1008
tamsargsyan <62563325+tamsargsyan@users.noreply.github.com>
2020-06-29 09:54:42 +0400

    add some color changes btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-let color = ["#f0dde8", "#ddf097", "#9a936f", "#d3c0c2", "#4696D3"];
+let color = [
+  "#222f3e",
+  "#f0dde8",
+  "#ddf097",
+  "#9a936f",
+  "#d3c0c2",
+  "#4696D3",
+  "#46D300",
+  "#D384C7",
+  "#feea54",
+  "#619a8b",
+  "#a5bdd9",
+  "#4f989e",
+];
 let i = 0;
 document.querySelector("button").addEventListener("click", function () {
   i = i < color.length ? ++i : 0;
